6ce721974737fd2db5291fb2c8f0d78f1a6eec38
abcdefg202210-arch <abcdefg202210@gmail.com>
2026-04-03 22:38:05 +0800

    Create 20_Dex_Swap_Pair.sol

---
+++ 20_Dex_Swap_Pair.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract DexSwapPair {
+    function swap(uint256 amount) external pure returns (uint256) {
+        return amount / 100;
+    }
+}
